310bc7c586af7b1874313b139d1f920803a5971f
haing <hainguyen22k3@gmail.com>
2025-04-30 12:05:17 +0700

    Initial commit for mock API

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+// Thêm CORS
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 server.use(router);
+
+// In log để kiểm tra server khởi động
+console.log('JSON Server is running');
+
+// Khởi động server trên cổng 3000
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
 
 module.exports = server;
